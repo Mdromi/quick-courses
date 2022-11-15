4fac6da0026ab3b760f0a409b349456f25aa0c42
Mdromi <mdromi14@gmail.com>
2022-11-16 01:50:41 +0600

    C # added

---
+++ C#/CodeWithHarry C# Crush Course/app/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // int romi = 19;
+            // Console.WriteLine("Hello, World!"+ romi);
+            // Console.ReadLine();
+            // string inp = Console.ReadLine();
+            // Console.WriteLine(inp);
+
+            // Data type Examples
+            // int a = 34;
+            // float b = 34.4F;
+            // double c = 34.4D;
+            // bool isGreat = true;
+            // char d = 'D';
+            // string e = "This is a String";
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            // Console.WriteLine(c);
+            // Console.WriteLine(isGreat);
+            // Console.WriteLine(d);
+            // Console.WriteLine(e);
+
+            // Type Casting
+            // char to int to long to float to double
+            // int x = (int) 3.5;
+            // int x1 = 3;
+            // double y = x;
+            // int z = 'y';
+            // Console.WriteLine(x);
+            // Console.WriteLine(x1);
+            // Console.WriteLine(y);
+            // Console.WriteLine(z);
+
+            // float varr = Convert.ToInt64(3.55);  // Convert.ToDouble, Convert.ToString;
+            // string sx = "34 is amazing";
+            // Console.WriteLine(varr);
+
+            // Taking User Input
+            // Console.WriteLine("Enter Your name");
+            // string name = Console.ReadLine();
+            // Console.WriteLine("hey, hello " + name);
+
+            // Console.WriteLine("How many need candy: ");
+            // string candy = Console.ReadLine();
+            // Console.WriteLine("you get " + (Convert.ToInt64(candy) + 4));
+
+            // Operators in C#
+            // int a = 20; 
+            // int b = 10;
+            // Console.WriteLine("The Value of a + b is: " + (a + b));
+            // Console.WriteLine("The Value of a - b is: " + (a - b));
+            // Console.WriteLine("The Value of a * b is: " + (a * b));
+            // Console.WriteLine("The Value of a / b is: " + (a / b));
+            // Console.WriteLine("The Value of a % b is: " + (a % b));
+
+            // Console.WriteLine("The Value of (a += b) is: " + (a += b));
+            // Console.WriteLine("The Value of (a -= b) is: " + (a -= b));
+            // Console.WriteLine("The Value of (a *= b) is: " + (a *= b));
+            // Console.WriteLine("The Value of (a /= b) is: " + (a /= b));
+
+            // Console.WriteLine(true && false); // false
+            // Console.WriteLine(false && false); // false
+            // Console.WriteLine(true && true); // true
+
+            // Console.WriteLine(true || false); // true
+            // Console.WriteLine(false || false); // false
+
+            // Console.WriteLine(!false); // true
+            // Console.WriteLine(!true); // false
+
+
+            // Console.WriteLine(5 == 5);
+            // Console.WriteLine(7 > 5);
+            // Console.WriteLine(5 >= 5);
+            // Console.WriteLine(4 < 5);
+            // Console.WriteLine(6 <= 5);
+            // Console.WriteLine(5 != 5);
+
+
+            // math class in c#
+            // int a = Math.Max(34, 345); 
+            // int b = Math.Min(34, 345);
+            // double c = Math.Sqrt(39);
+            // double d = Math.Abs(-39);
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            // Console.WriteLine(c);
+            // Console.WriteLine(d);
+
+            // string method
+            // string hello = "Hello World this is Romi";
+            // Console.WriteLine(hello.Length);
+            // Console.WriteLine(hello.ToUpper());
+            // Console.WriteLine(hello.ToLower());
+            // Console.WriteLine(string.Concat(hello, "You are nice"));
+
+            // string name = Console.
+        }
+    }
+}
